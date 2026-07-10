9b9f4cc65dd7af196adf01b92033e039ce0d4f36
Anirban-dev <65975377+Anirban-dev@users.noreply.github.com>
2026-07-10 17:09:21 +0530

    some fixes

--- AiCalls/main.py
+++ AiCalls/main.py
@@ -32,4 +32,4 @@ app.include_router(llm_router)
 
 if __name__ == "__main__":
     import uvicorn # type: ignore
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
